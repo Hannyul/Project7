268252a2288f787b73d7d356a1d274e74e1366de
avocado-toast99 <37838166+avocado-toast99@users.noreply.github.com>
2018-11-26 22:40:41 +0300

    matrix like miss ebtissam's slides

---
+++ GaleShapely.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package galeshapely;
+
+/**
+ *
+ * @author maha
+ */
+public class GaleShapely {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        String[][] MalePreferences = {
+            // Andy's preferences:
+            {"Olivia", "Madison", "Natalie", "Lucy", "Peyton"},
+            // Brad's preferences:
+            {"Peyton", "Natalie", "Madison", "Lucy", "Olivia"},
+            // Caleb's preferences:
+            {"Madison", "Peyton", "Lucy", "Olivia", "Natalie"},
+            // Daniel's preferences
+            {"Petyon", "Madison", "Olivia", "Natalie", "Lucy"},
+            // Ethan's preferences:
+            {"Olivia", "Lucy", "Madison", "Natalie", "Petyon"}
+
+        };
+
+        String[][] FemalePreferences = {
+            // Lucy's preferences:
+            {"Daniel", "Brad", "Ethan", "Caleb", "Andy"},
+            // Madison's preferences:
+            {"Brad", "Andy", "Daniel", "Caleb", "Ethan"},
+            // Natalie's preferences:
+            {"Andy", "Caleb", "Ethan", "Daniel", "Brad"},
+            // Olivia's preferences:
+            {"Daniel", "Andy", "Caleb", "Brad", "Ethan"},
+            // Peyton's preferences:
+            {"Brad", "Ethan", "Andy", "Caleb", "Daniel"}
+
+        };
+
+    }
+
+}
